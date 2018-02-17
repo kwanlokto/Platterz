06181d0626cd688e019edf0cec00981b03aab703
Lok To Kwan <ray.kwan@mail.utoronto.ca>
2018-02-17 08:17:04 -0500

    editted main

--- app/src/main/java/platerz/hackathon/Platterz.java
+++ app/src/main/java/platerz/hackathon/Platterz.java
@@ -3,22 +3,41 @@ package platerz.hackathon;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-
-public class Platterz extends AppCompatActivity {
+import android.widget.Button;
+import android.support.v7.widget.*;
+import android.view.*;
+public class Platterz extends AppCompatActivity implements View.OnClickListener{
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
-
+    public static User user;
+    public static RecipeBook cookBook; // NEED TO ADD ALL RECIPESS
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_platterz);
+        setContentView(R.layout.opening_page);
+
+        //Add all actionlisteners to buttons
+        final Button login = findViewById(R.id.Login);
+        login.setOnClickListener(this);
+        final Button signUp = findViewById(R.id.signUp);
+        signUp.setOnClickListener(this);
+        final Button beginner = findViewById(R.id.Beginner);
+        beginner.setOnClickListener(this);
+        final Button average = findViewById(R.id.Average);
+        average.setOnClickListener(this);
+        final Button gourmet = findViewById(R.id.Gourmet);
+        gourmet.setOnClickListener(this);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(myToolbar);
+
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        //TextView tv = (TextView) findViewById(R.id.sample_text);
+        //tv.setText(stringFromJNI());
     }
 
     /**
@@ -26,4 +45,36 @@ public class Platterz extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    @Override
+    public void onClick(View view) {
+        //If the login Button is clicked
+        if (view.getId() == R.id.Login) {
+            boolean found = false;
+
+            //NEED TO STORE USER NAMES
+            if (found) {
+                setContentView(R.layout.swipe_recipes);
+            }
+        }
+        //If we are signing up
+        else if (view.getId() == R.id.signUp) {
+            String name = ((TextView)findViewById(R.id.UserName)).getText().toString();
+            String password = ((TextView)findViewById(R.id.Password)).getText().toString();
+            user = new User(name, password);
+            setContentView(R.layout.register_exp);
+        }
+        else if (view.getId() == R.id.Beginner) {
+            user.setExp("Beginner");
+        }
+        else if (view.getId() == R.id.Average) {
+            user.setExp("Average");
+        }
+        else if (view.getId() == R.id.Gourmet) {
+            user.setExp("Gourmet");
+        }
+        else if (view.getId() == R.id.Add) {
+            //user.addRecipe();
+        }
+    }
 }
